33c95ac17a7195575ce5018e234528e890b4158e
damian <damian@omnimedia.co.nz>
2015-10-29 20:08:42 +1300

    modify controller

--- public/js/controllers/PoolController.js
+++ public/js/controllers/PoolController.js
@@ -13,60 +13,20 @@ app.controller('poolController', function($scope, $http) {
 	}
 
 });
-
-app.controller('loginController', function($scope, $http, Users) {
-
-	$scope.pools = [];
-
-$scope.login = function() {
-			var success = true;
-
-			// save the comment. pass in comment data from the form
-			// use the function we created in our service
-			Users.login($scope.authCredentials)
-				.success(function(data) {
-					
-					if(data){
-					// $location.path('/admin');
-					// if (!$rootScope.$$phase) $rootScope.$apply();
-					console.log(data);
-				}
-
-					// if successful, we'll need to refresh the comment list
-					// Users.get()
-					// 	.success(function(getData) {
-					// 		$scope.users = getData;
-					// 		$scope.loading = false;
-					// 	});
-
-				})
-				.error(function(data) {
-					console.log(data);
-				});
-		};
-	$scope.addVote = function(pooloptions) {
-		$http.get('/api/pooloption/addvote/' + pooloptions.id).
-		success(function(data, status, headers, config) {
-			pooloptions.vote++;
-		});
-	}
-
-});
-
 app.controller('adminController', function($scope, $http) {
 
 	$scope.pools = [];
-	$scope.pools.pool = {};
+	$scope.newpool = { };
 	$http.get('/api/pool').
 	success(function(data, status, headers, config) {
 		$scope.pools = data;
 	});
 	$scope.addPool = function() {
-		$http.post('/api/pool', $scope.pools.pool).
+		$http.post('/api/pool', $scope.newpool).
 		success(function(data, status, headers, config) {
 			data.pooloptions=[];
 			$scope.pools.push(data);
-			$scope.pools.pool.title="";
+			$scope.newpool.title="";
 		});
 	}
 	$scope.removePool = function(pool) {
@@ -79,19 +39,19 @@ app.controller('adminController', function($scope, $http) {
 			}
 		});
 	}
-	$scope.addOption = function(pool,pooloptions) {
-		pooloptions.pool_id=pool.id;
-		$http.post('/api/pooloption', pooloptions).
+	$scope.addOption = function(pool,newoption) {
+		newoption.pool_id=pool.id;
+		$http.post('/api/pooloption', newoption).
 		success(function(data, status, headers, config) {
 			pool.pooloptions.push(data);
-			pooloptions.title='';
+			newoption.title='';
 		});	
 	}
-	$scope.removeOption = function(pooloption, pool) {
-		$http.delete('/api/pooloption/' + pooloption.id).
+	$scope.removeOption = function(pooloptions, pool) {
+		$http.delete('/api/pooloption/' + pooloptions.id).
 		success(function(data, status, headers, config) {
 			for (index = 0; index < pool.pooloptions.length; ++index) {
-				if (pool.pooloptions[index].id == pooloption.id) {
+				if (pool.pooloptions[index].id == pooloptions.id) {
 					pool.pooloptions.splice(index, 1);
 				}
 			}
